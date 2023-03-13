0de9558d95431aed19bf08b338b36700819c336a
PavelVanin1996 <pavelvani@gmail.com>
2023-03-12 18:16:25 -0600

    Changed fw and bw direction

--- server/move.py
+++ server/move.py
@@ -22,11 +22,11 @@ Motor_B_Pin2  = 18
 Dir_forward   = 1 # changing forward and backward direction
 Dir_backward  = 0
 
-left_forward  = 0
-left_backward = 1
+left_forward  = 1# changing forward left and backward left direction
+left_backward = 0
 
-right_forward = 0
-right_backward= 1
+right_forward = 1 # changing forward right and backward right direction
+right_backward= 0
 
 pwn_A = 0
 pwm_B = 0
